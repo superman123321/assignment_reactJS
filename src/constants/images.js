69ae88025351f48861ddde7620eb954f6ef5ad57
KhoaNH <Khoa@ominext.com>
2023-07-03 12:06:10 +0700

    update code

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,50 +1,19 @@
-import landscape from '../assets/img/background.jpg'
-import landscape2 from '../assets/img/landscape2.jpeg'
-import landscape3 from '../assets/img/landscape3.jpeg'
-import landscape4 from '../assets/img/landscape4.jpeg'
-import landscape5 from '../assets/img/landscape5.jpeg'
-import landscape6 from '../assets/img/landscape6.jpeg'
-import animal from '../assets/img/animal1.webp'
+import landscape from "../assets/img/background.jpg";
+import landscape2 from "../assets/img/landscape2.jpeg";
+import landscape3 from "../assets/img/landscape3.jpeg";
+import landscape4 from "../assets/img/landscape4.jpeg";
+import landscape5 from "../assets/img/landscape5.jpeg";
+import landscape6 from "../assets/img/landscape6.jpeg";
+import animal from "../assets/img/animal1.webp";
 
+const Image = [
+  { photo: landscape, categoryId: "1", title: "Premium", id: "1" },
+  { photo: landscape2, categoryId: "2", title: "Premium", id: "2" },
+  { photo: landscape3, categoryId: "3", title: "Premium", id: "3" },
+  { photo: landscape4, categoryId: "4", title: "Premium", id: "4" },
+  { photo: landscape5, categoryId: "5", title: "Premium", id: "5" },
+  { photo: landscape6, categoryId: "6", title: "Premium", id: "6" },
+  { photo: animal, id: "7", categoryId: "7", title: "Premium" },
+];
 
-
-const Image =[
-  {photo: landscape,
-   categoryId:'1',
-   title:'Premium',
-      id: '0001',
-  },
-  {photo: landscape2,
-   categoryId:'2',
-   title:'Premium',
-     id:'0002' 
-  },
-  {photo: landscape3,
-   categoryId:'3',
-   title:'Premium',
-     id:'0003' 
-  },
-  {photo: landscape4,
-   categoryId:'4',
-   title:'Premium',
-     id:'0004' 
-  },
-  {photo: landscape5,
-   categoryId:'5',
-   title:'Premium',
-     id:'0005' 
-  },
-  {photo: landscape6,
-   categoryId:'6',
-   title:'Premium',
-     id:'0006' 
-  },
-  {photo: animal,
-     id:'0007',
-     categoryId:'7',
-     title:'Premium',
-  }
-
-]
-
-export default Image
+export default Image;
